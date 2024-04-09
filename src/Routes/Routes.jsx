56ca90a5cf23d6499bdf10ecdf64d5a2f8ee9495
aayman01 <ayman.alfi098@gmail.com>
2024-04-10 04:31:01 +0600

    error page done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Home from "../pages/Home";
 import Login from "../pages/Login/Login";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 path: '/',
